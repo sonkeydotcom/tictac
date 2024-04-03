31f7800ea7d740da193f91111a079bc631b0f00d
sonkey <annagu.kennedy@gmail.com>
2024-04-03 09:47:50 +0100

    dependecies

--- app/app/socketService.js
+++ app/app/socketService.js
@@ -1,11 +1,12 @@
 import io from "socket.io-client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const socket = io("http://192.168.0.5:3000/", { transports: ["websocket"] });
+const socket = io("https://tictac-fle8.onrender.com/", {
+  transports: ["websocket"],
+});
 
 socket.on("connect", () => {
   console.log("Connected to server");
 });
-
 
 export default socket;
